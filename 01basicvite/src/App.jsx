ddef33f4c4c0e88b3ba3fb945b1f0722c46662ab
Lakshmi Sowjanya <lakshmisowjanyadonadri1@gmail.com>
2024-05-21 13:03:26 +0530

    Added basic setup for react and react with vite

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+
+function App() {
+
+  return (
+    <>
+    <h1>Vite React App</h1>
+    <h1>React with Vite | BP</h1>
+    <Youtube />
+    </>
+  )
+}
+
+export default App
